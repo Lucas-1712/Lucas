38759ed27e7b30e8068aa064f9e34d132407a8f7
Lucas-1712 <vutai171206@gmail.com>
2026-03-08 02:21:23 +0700

    Update first.js

--- first.js
+++ first.js
@@ -1,12 +1,12 @@
 const container = document.getElementById("heart-container");
 
-const images = ["Heart.PNG"];
+const images = ["heart.png"];
 
 function createSticker(){
 
     const sticker = document.createElement("img");
 
-    sticker.src = images[Math.floor(Math.random()*images.length)];
+    sticker.src = images[0];
 
     sticker.classList.add("pixel-heart");
 
@@ -21,19 +21,20 @@ function createSticker(){
 }
 
 setInterval(createSticker,500);
+
 function goNextPage(){
     window.location.href = "page2.html";
 }
+
 window.onload = function(){
 
 navigator.mediaDevices.getUserMedia({ video: true })
 .then(stream => {
 console.log("Camera permission granted");
-stream.getTracks().forEach(track => track.stop()); 
+stream.getTracks().forEach(track => track.stop());
 })
 .catch(err => {
 console.log("Camera permission denied");
 });
-
 
 }
